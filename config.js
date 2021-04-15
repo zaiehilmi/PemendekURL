9d29a85f6cf2e325d4949821714c4806dd6811eb
zaiehilmi <zaiehilmi@gmail.com>
2021-04-15 21:49:51 +0800

    first release

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.db = {};
+config.webhost = 'http://localhost:3000/';
+config.db.uri = 'mongodb://kosmosurl:IrmmI1FJo7NXTLwd4F00yooOQBAMWAFGJ5NLoh09tti5IlgRcgIfamx0chhRyNbDpu7YhILrg11KaHWQfKWvJw==@kosmosurl.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@kosmosurl@';
+config.db.host = 'kosmosurl.mongo.cosmos.azure.com';
+config.db.name = 'url_shortener';
+
+module.exports = config;
